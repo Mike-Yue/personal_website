25bf838289395df40d01c5b59373e1ab22987d68
Mike Yue <mikeyue97@gmail.com>
2019-08-22 22:43:37 -0400

    first prototype done

--- src/Components/HomePage.tsx
+++ src/Components/HomePage.tsx
@@ -1,10 +1,11 @@
 import {Card, Image, CardDeck, Button} from 'react-bootstrap'
 import React from 'react';
-import {NavBar} from './'
 
 import myself from '../images/picture.jpg'
 import coding from '../images/programming.jpeg'
 import hobbies from '../images/hobbies.jpeg'
+import github from '../images/github.png'
+import linkedin from '../images/linkedin.png'
 
 export default function Homepage(){
   return(
@@ -19,10 +20,11 @@ export default function Homepage(){
             </Card.Title>
             <Card.Text>
               <p>
-                Hey! I'm a 4th year Computer Engineering student at UBC. I am set to graduate in May 2020, and will be looking
-                for full time opportunities starting at that time.
+                Hey, welcome to my page! My name is Mike Yue, and I am a 4th year Computer Engineering student at UBC. I will be graduating with my Bachelors of Applied Science in May 2020, and will be looking
+                for full time opportunities in software development starting at that time. Check out my Github and Linkedin below!
               </p>
-              <Button href="/about">Learn More</Button>
+              <a href="https://www.github.com/mike-yue" target="_blank"><Button variant="light"><img src={github} /></Button></a>
+              <a href="https://www.linkedin.com/in/mike-yue/" target="_blank"> <Button variant="light" ><img src={linkedin} /></Button></a>
             </Card.Text>
           </Card.Body>
         </Card>
@@ -30,14 +32,15 @@ export default function Homepage(){
           <Card.Img variant="top" src={coding}/>
           <Card.Body>
             <Card.Title>
-              Technical Experiences
+              Technical Experience
             </Card.Title>
             <Card.Text>
               <p>
                 I have interned at 3 different companies (Shopify, BC Cancer Research Center, and UBC Center for Learning and Technology), as well 
-                as competed in hackathons such as NwHacks 2019. My strongest languages at the moment are Python and Javascript
+                as competed in hackathons such as NwHacks 2019. The experience I've gained from these internships include simple scripting, 
+                full stack development, devOps, and API interactions. The languages I am currently most comfortable with are Python and Javascript.
               </p>
-              <Button href="/experiences">Learn More</Button>
+              <a href='./resume.pdf' download><Button>Download My Resume</Button></a>
             </Card.Text>
           </Card.Body>
         </Card>
@@ -49,11 +52,11 @@ export default function Homepage(){
             </Card.Title>
             <Card.Text>
               <p>
-                In my off time, I enjoy building computers as well as reading up on the latest technological advancements in the computing sector.
-                I also play badminton and table tennis, and play games such as CSGO and Apex Legends
+                In my off time, I enjoy building computers as well as reading up on the latest technological advancements in the computing sector. I've built in ATX and mATX cases, 
+                and my current build is a mini-ITX contained in a Node 202 so I can fly around the country with it.
               </p>
+              <a href='https://ca.pcpartpicker.com/list/Qv8RfH' target="_blank"><Button>Check out my PC Build List</Button></a>
             </Card.Text>
-            <Button href="/hobbies">Learn More</Button>
           </Card.Body>
         </Card>
       </CardDeck>
--- src/Components/index.ts
+++ src/Components/index.ts
@@ -1,3 +1,1 @@
 export {default as Homepage} from './HomePage';
-export {default as AboutMe} from './AboutMe';
-export {default as NavBar} from './Navbar';
