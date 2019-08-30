9ce0d5a947165b4a2abae7d325699ac9da9d4ea1
Mike Yue <mikeyue97@gmail.com>
2019-08-29 20:21:59 -0700

    Updates

--- src/Components/index.ts
+++
@@ -1,4 +0,0 @@
-export {default as Homepage} from './HomePage';
-export {default as Languages} from './Languages';
-export {default as Hobbies} from './Hobbies';
-export {default as Navigation} from './Navigation';
